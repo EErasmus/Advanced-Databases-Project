913d713fc9785b322751b7cd282a7edfb10d0d87
yash <yashvardhan.bhardwaj@outlook.com>
2020-10-29 14:43:09 +0100

    updating faculty_id key in courses.

--- Assignment/Assignment 3/test courses script.sql
+++ Assignment/Assignment 3/test courses script.sql
@@ -1,5 +1,5 @@
 create table t as 
-select course_master.*,faculty_test.university_id  from course_master cross join 
+select course_master.*,faculty_test.faculty_id  from course_master cross join 
 faculty_test
 
 
